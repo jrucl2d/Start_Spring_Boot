79621e4c6b075c641bd16654da029918c6102922
jrucl2d <ysk789465@gmail.com>
2020-12-11 15:43:24 +0900

    댓글 CRUD 컨트롤러 작업 완료

--- project07/src/main/java/com/yu/project06/repository/ReplyRepository.java
+++ project07/src/main/java/com/yu/project06/repository/ReplyRepository.java
@@ -1,7 +1,14 @@
 package com.yu.project06.repository;
 
+import com.yu.project06.domain.Board;
 import com.yu.project06.domain.Reply;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends CrudRepository<Reply, Long> {
+
+    @Query("SELECT r FROM Reply r WHERE r.board = ?1 AND r.rno > 0 ORDER BY r.rno ASC")
+    public List<Reply> getRepliesOfBoard(Board board);
 }
