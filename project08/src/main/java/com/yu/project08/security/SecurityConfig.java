0da9b2c854361bd1f252bb18102d375f22ae0e41
jrucl2d <ysk789465@gmail.com>
2020-12-12 16:50:15 +0900

    권한 관련 로그인 처리, 로그아웃 처리완료

--- project08/src/main/java/com/yu/project08/security/SecurityConfig.java
+++ project08/src/main/java/com/yu/project08/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.yu.project08.security;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +15,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         log.info("security config...................");
+
+        http.authorizeRequests().antMatchers("/guest/**").permitAll();
+        http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
+        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+
+        http.formLogin().loginPage("/login");
+
+        // Authorize에 실패했을 때 보여주는 안내창
+        http.exceptionHandling().accessDeniedPage("/accessDenied");
+
+        // 로그아웃(세션 무효화)
+        http.logout().logoutUrl("/logout").invalidateHttpSession(true); // deleteCookies()를 사용할 수도 있음
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        log.info("build Auth global......................");
+
+        auth.inMemoryAuthentication()
+                .withUser("manager")
+                .password("1111")
+                .roles("MANAGER");
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new PasswordEncoder() {
